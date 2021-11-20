4e9af4529d67e4dd1ff2b6c60b5aa8cd2920ca62
jqt3of5 <jqt3of5@gmail.com>
2021-11-20 14:31:38 -0700

    Added a few real items to the side bar, and stubbing out the views

--- laser-admin/src/App.tsx
+++ laser-admin/src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import SideBarMenu from "./SideBarMenu";
+import SideBarMenu from "./Components/SideBarMenu";
+import ProjectDetailView from "./Views/ProjectDetailView";
+import ProjectList from "./Views/ProjectList";
+import MaterialsList from "./Views/MaterialsList";
 
 function App() {
+    const [selected, setSelected] = useState("Projects")
+
+    let currentView = null
+    switch(selected)
+    {
+        case "Projects":
+            currentView = <ProjectList></ProjectList>
+            break;
+        case "Materials":
+            currentView = <MaterialsList></MaterialsList>
+            break;
+        case "Users":
+            break;
+        case "Settings":
+            break;
+        case "Analytics":
+            break;
+    }
   return (
     <div className="App">
         <header className="App-header">
@@ -11,10 +32,10 @@ function App() {
             <div className={"App-side-bar"}>
                 <div className={"side-bar-header"}>
                 </div>
-                <SideBarMenu selected={""} items={["Item1", "Item2", "Item3"]} onSelect={(item, index) => console.log(item)}/>
+                <SideBarMenu selected={selected} items={["Projects", "Materials", "Users", "Settings", "Analytics"]} onSelect={(item, index) => setSelected(item)}/>
             </div>
             <div className={"main-view"}>
-
+                {currentView}
             </div>
         </div>
     </div>
---
+++ laser-admin/src/Views/ProjectList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface ProjectListProps{
+}
+export default function ProjectList(props: ProjectListProps) {
+    return <div>ProjectsList</div>
+}
