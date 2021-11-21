d39dfe9748cf883244e05173f89b5f1758781f04
jqt3of5 <jqt3of5@gmail.com>
2021-11-21 16:08:09 -0700

    Building out the Projects/Orders table

--- laser-admin/src/App.tsx
+++ laser-admin/src/App.tsx
@@ -6,13 +6,26 @@ import ProjectList from "./Views/ProjectList";
 import MaterialsList from "./Views/MaterialsList";
 
 function App() {
+    const data = React.useMemo(() => [
+        {
+            name: "John Todd",
+            email: "jqt3of5@gmail.com",
+            address: "3423 woodhouse",
+            phoneNumber: "234-234-23423",
+            cost: 123.34,
+            orderid: "1234567",
+            status: 0,
+            projectGuid: "1234qwerty",
+            projectUrl: "http://asdfasdf/1234qwerty"
+        }
+        ],[])
     const [selected, setSelected] = useState("Projects")
 
     let currentView = null
     switch(selected)
     {
         case "Projects":
-            currentView = <ProjectList></ProjectList>
+            currentView = <ProjectList orders={data}></ProjectList>
             break;
         case "Materials":
             currentView = <MaterialsList></MaterialsList>
@@ -27,14 +40,15 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
+            <div>CraftCloset Admin</div>
         </header>
         <div className={"App-content"}>
             <div className={"App-side-bar"}>
                 <div className={"side-bar-header"}>
                 </div>
-                <SideBarMenu selected={selected} items={["Projects", "Materials", "Users", "Settings", "Analytics"]} onSelect={(item, index) => setSelected(item)}/>
+                <SideBarMenu selected={selected} items={["Projects", "Materials", "Users", "Analytics"]} onSelect={(item, index) => setSelected(item)}/>
             </div>
-            <div className={"main-view"}>
+            <div className={"App-main-view"}>
                 {currentView}
             </div>
         </div>
