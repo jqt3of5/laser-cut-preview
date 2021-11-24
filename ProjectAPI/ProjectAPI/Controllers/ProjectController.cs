212afab5b22247de6adeb1796f48b8c472075ac0
jqt3of5 <jqt3of5@gmail.com>
2021-11-23 21:49:59 -0700

    moving things all over the place

---
+++ ProjectAPI/ProjectAPI/Controllers/ProjectController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Data;
+using LaserPreview.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LaserPreview.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ILogger<ProjectController> _logger;
+        private readonly ProjectRepo _repo;
+
+        public ProjectController(ILogger<ProjectController> logger, ProjectRepo repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        [HttpGet("{projectId}")]
+        public Project GetProject(string projectId)
+        {
+            return _repo.GetProject(projectId);
+        }
+
+        [HttpPost("{projectId}")]
+        public IActionResult SaveProject([FromBody] Project project)
+        {
+            if (_repo.IsProjectReadonly(project.projectId))
+            {
+                //Not Modified
+                return StatusCode(304, _repo.GetProject(project.projectId));
+            }
+            return Ok(_repo.SaveProject(project));
+        }
+
+        [HttpPost("{projectId}/order")]
+        public IActionResult OrderProject(string projectId, [FromBody] Customer customer)
+        {
+            if (_repo.IsProjectReadonly(projectId))
+            {
+                return StatusCode(304, "Project already ordered");
+            }
+
+            if (!_repo.OrderProject(projectId, customer))
+            {
+                return StatusCode(304, "Order failed");
+            }
+            return Ok();
+        }
+    }
+}
