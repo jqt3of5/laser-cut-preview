72fee794b45b925eb639387a588774a74452c569
John Todd <jqt3of@gmail.com>
2021-12-12 13:51:21 -0700

    Trying to fix https

--- ProjectAPI/ProjectAPI/Startup.cs
+++ ProjectAPI/ProjectAPI/Startup.cs
@@ -40,6 +40,7 @@ namespace LaserPreview
         {
             if (env.IsDevelopment())
             {
+                app.UseHttpsRedirection();
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjectAPI v1"));
@@ -54,11 +55,10 @@ namespace LaserPreview
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
            
         
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
